5021ad2240ccf4834c268a2450fb11ff5973df52
Birkin James Diana <birkin.diana@gmail.com>
2024-08-30 19:16:55 -0400

    updates module-name.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from .create_foo import cli as create_cli
+from .count_collections import cli as count_collections_cli
 from .update_foo import cli as update_cli
 
 
@@ -7,9 +7,9 @@ def main():
     if len(sys.argv) > 1:
         command = sys.argv[1]
         args: list = sys.argv[2:]  # Collect all the additional arguments after the command
-        if command == 'create_foo':
-            print( 'in create_foo.main.main(); calling create_foo.cli.run()' )
-            create_cli.run(args)
+        if command == 'count_collections':
+            print( 'in count_collections.main.main(); calling count_collections.cli.run()' )
+            count_collections_cli.run(args)
         elif command == 'update_foo':
             print( 'in update_foo.main.main(); calling update_foo.cli.run()' )
             update_cli.run(args)
