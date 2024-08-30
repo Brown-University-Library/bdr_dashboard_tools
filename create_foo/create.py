20669dc583b419b2fedb5b855471b5fa1fcfe00b
Birkin James Diana <birkin.diana@gmail.com>
2024-08-30 18:35:55 -0400

    adds gitignore.

--- create_foo/create.py
+++ create_foo/create.py
@@ -1,5 +1,6 @@
-import utils
+from .utils import helper_function
 
 def create( data: str ):
-    print( f'in create_foo.create.create(); creating with data: ``{data}``')
-    processed_data = utils.helper_function( data )
+    print( f'in create_foo.create.create(); data: ``{data}``')
+    processed_data = helper_function( data )
+    print( f'back in create_foo.create.create(); processed_data: ``{processed_data}``' )
--- create_foo/utils.py
+++ create_foo/utils.py
@@ -1,3 +1,5 @@
 def helper_function( data: str ) -> str:
     print( 'in create_foo.utils.helper_function()' )
-    return data.upper()
+    data = data.upper()
+    print( f'...data is now: ``{data}``' )
+    return data
