a8a062d11fef7f980afc7a3b7d89dc472ad660c3
Birkin James Diana <birkin.diana@gmail.com>
2024-09-10 18:48:51 -0400

    differentially prints out json or columnar data.

--- code_to_package/count_collections/manager.py
+++ code_to_package/count_collections/manager.py
@@ -3,7 +3,7 @@ Manager module.
 Calls utils.helper_function() if needed
 """
 
-import logging
+import json, logging, pprint
 from columnar import columnar
 from .utils import helper_function, get_bdr_collections
 
@@ -12,10 +12,20 @@ log = logging.getLogger( __name__ )
 
 
 def manage_processing( data: str ):
+    """ Manages the processing.
+        Code credit: <https://github.com/contrick64>
+        Called by cli.run() with the parsed args. """
     log.debug( f'in create_foo.manage.manage_processing(); data: ``{data}``')
     bdr_collections: dict = get_bdr_collections()
 
-    colls_table = [[key,value] for key,value in bdr_collections.items()]
-    table_headers = ['collection','no. items']
-    log.debug(columnar(colls_table,headers=table_headers,no_borders=True))
-    log.debug(f'Total number of collections: {len(bdr_collections)}')    
+    if data == 'json':
+        info_dict = { 
+            'data': bdr_collections, 
+            'info': 'Top 100 collections, by number of items.',  }
+        data = json.dumps( info_dict, indent=2 )  # don't sort keys, keep the data in the order-by-count
+        print( data )
+    else:
+        colls_table = [ [key,value] for key,value in bdr_collections.items() ]
+        table_headers = [ 'collection','no. items' ]
+        print(columnar( colls_table,headers=table_headers,no_borders=True) )
+        print( f'Total number of collections: {len(bdr_collections)}' )    
