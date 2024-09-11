dcaba41cde9564ca52776ac93599c33db565ed9a
Birkin James Diana <birkin.diana@gmail.com>
2024-09-11 00:06:02 -0400

    parses search-collecton-name args.

--- code_to_package/search_collection_name/cli.py
+++ code_to_package/search_collection_name/cli.py
@@ -15,7 +15,7 @@ def run( args: list ) -> None:
     log.debug( 'preparing arg-parser' )
     parser = argparse.ArgumentParser( description='Parses search_collection_name arguments' )
     parser.add_argument( '--collection_name', required=False, help='collection name to search for' )
-    parser.add_argument( '--collection_json_array_file', required=False, help='input.json file, of array of names, to load' )
+    parser.add_argument( '--collection_json_array_filepath', required=False, help='input.json file, of array of names, to load' )
     parser.add_argument( '--output', required=True, help='"json" or "columnar"' )
     parsed_args = parser.parse_args(args)
     ## validate output arg ------------------------------------------
@@ -24,12 +24,16 @@ def run( args: list ) -> None:
         msg = f'Invalid data format: `{output_data_format}`; output should be "json" or "columnar"' 
         raise ValueError( msg )
     ## validate collection args -------------------------------------
-    if parsed_args.collection_name and parsed_args.collection_json_array_file:
-        msg = 'Only one of --collection_name or --collection_json_array_file should be provided'
+    collection_name = parsed_args.collection_name
+    collection_json_array_filepath = parsed_args.collection_json_array_filepath
+    if collection_name and collection_json_array_filepath:
+        msg = 'Only one of --collection_name or --collection_json_array_filepath should be provided'
         raise ValueError( msg )
-    if not parsed_args.collection_name and not parsed_args.collection_json_array_file:
-        msg = 'One of --collection_name or --collection_json_array_file should be provided'
+    if not collection_name and not collection_json_array_filepath:
+        msg = 'One of --collection_name or --collection_json_array_filepath must be provided'
         raise ValueError( msg )
+    if collection_json_array_filepath:
+        print( 'Sorry, the collection_json_array_filepath option is not yet implemented.' )
     ## call manager function ----------------------------------------
-    manage_processing( output_data_format )  # Pass the parsed data to the create function
+    manage_processing( collection_name, collection_json_array_filepath, output_data_format )  # Pass the parsed data to the create function
     return
--- code_to_package/search_collection_name/manager.py
+++ code_to_package/search_collection_name/manager.py
@@ -10,10 +10,23 @@ from .utils import helper_function
 log = logging.getLogger( __name__ )
 
 
-def manage_processing( data: str ):
+def manage_processing( collection_name: str, collection_json_array_filepath: str, output_data_format: str ):
     """ Manages processing.
         Called by cli.run() with the parsed args. """
     log.debug( 'about to call helper-function' )
-    processed_data: str = helper_function( data )
-    log.debug( f'back in manage_processing(); processed_data: ``{processed_data}``' )
+    # processed_data: str = helper_function( data )
+    log.debug( f'collection_name, ``{collection_name}``' )
+    log.debug( f'collection_json_array_filepath, ``{collection_json_array_filepath}``' )
+    log.debug( f'output_data_format, ``{output_data_format}``' )
+    return
+
+
+
+
+# def manage_processing( data: str ):
+#     """ Manages processing.
+#         Called by cli.run() with the parsed args. """
+#     log.debug( 'about to call helper-function' )
+#     processed_data: str = helper_function( data )
+#     log.debug( f'back in manage_processing(); processed_data: ``{processed_data}``' )
     
