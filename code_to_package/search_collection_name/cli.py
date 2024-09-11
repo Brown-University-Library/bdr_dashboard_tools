c322a790403794710304489e2d2294067e5e6463
Birkin James Diana <birkin.diana@gmail.com>
2024-09-10 23:36:45 -0400

    refactors cli code.

--- code_to_package/search_collection_name/cli.py
+++ code_to_package/search_collection_name/cli.py
@@ -1,17 +1,16 @@
 import argparse, logging
-from .update import update
+from .manager import manage_processing
 
 
 log = logging.getLogger( __name__ )
 
 
 def run( args: list ) -> None:
-    log.debug( 'in update_foo.cli.run(); preparing arg-parser' )
+    log.debug( 'preparing arg-parser' )
     parser = argparse.ArgumentParser(description="Update Foo Command")
-    parser.add_argument('--data', required=True, help='Data to update foo with')
+    parser.add_argument('--output', required=True, help='Data to update foo with')
     parsed_args = parser.parse_args(args)
-    data: str = parsed_args.data
+    output_data_format: str = parsed_args.output
 
-    log.debug( '...and about to call update_foo.upddate.update()' )
-    update( data )  # Pass the parsed data to the update function
+    manage_processing( output_data_format )  # Pass the parsed data to the update function
     return
--- code_to_package/search_collection_name/manager.py
+++ code_to_package/search_collection_name/manager.py
@@ -5,7 +5,7 @@ from .utils import helper_function
 log = logging.getLogger( __name__ )
 
 
-def update( data: str ):
+def manage_processing( data: str ):
     log.debug( f'in update_foo.update.update(); data: ``{data}``')
     processed_data = helper_function( data )
     log.debug( f'back in update_foo.update.update(); processed_data: ``{processed_data}``' )
