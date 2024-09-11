3150224342bc2db30acb102c0d7a28781f24c7b5
Birkin James Diana <birkin.diana@gmail.com>
2024-09-10 23:57:30 -0400

    enhances search_collection_name cli.

--- code_to_package/search_collection_name/cli.py
+++ code_to_package/search_collection_name/cli.py
@@ -14,11 +14,22 @@ log = logging.getLogger( __name__ )
 def run( args: list ) -> None:
     log.debug( 'preparing arg-parser' )
     parser = argparse.ArgumentParser( description='Parses search_collection_name arguments' )
-    parser.add_argument('--output', required=True, help='"json" or "columnar"')
+    parser.add_argument( '--collection_name', required=False, help='collection name to search for' )
+    parser.add_argument( '--collection_json_array_file', required=False, help='input.json file, of array of names, to load' )
+    parser.add_argument( '--output', required=True, help='"json" or "columnar"' )
     parsed_args = parser.parse_args(args)
+    ## validate output arg ------------------------------------------
     output_data_format: str = parsed_args.output
     if output_data_format not in ['json', 'columnar']:
         msg = f'Invalid data format: `{output_data_format}`; output should be "json" or "columnar"' 
         raise ValueError( msg )
+    ## validate collection args -------------------------------------
+    if parsed_args.collection_name and parsed_args.collection_json_array_file:
+        msg = 'Only one of --collection_name or --collection_json_array_file should be provided'
+        raise ValueError( msg )
+    if not parsed_args.collection_name and not parsed_args.collection_json_array_file:
+        msg = 'One of --collection_name or --collection_json_array_file should be provided'
+        raise ValueError( msg )
+    ## call manager function ----------------------------------------
     manage_processing( output_data_format )  # Pass the parsed data to the create function
     return
