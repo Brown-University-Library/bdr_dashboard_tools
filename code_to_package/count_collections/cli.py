34f1b9fc0ff93a1a73ccc2bddc9ae36d6a588467
Birkin James Diana <birkin.diana@gmail.com>
2024-09-10 23:44:21 -0400

    updates search_collection_name cli.

--- code_to_package/count_collections/cli.py
+++ code_to_package/count_collections/cli.py
@@ -12,8 +12,8 @@ log = logging.getLogger( __name__ )
 
 
 def run( args: list ) -> None:
-    log.debug( 'in create_foo.cli.run(); preparing arg-parser' )
-    parser = argparse.ArgumentParser( description="Create Foo Command" )
+    log.debug( 'preparing arg-parser' )
+    parser = argparse.ArgumentParser( description='Parses count_collections arguments' )
     parser.add_argument('--output', required=True, help='"json" or "columnar"')
     parsed_args = parser.parse_args(args)
     output_data_format: str = parsed_args.output
@@ -21,6 +21,5 @@ def run( args: list ) -> None:
     if output_data_format not in ['json', 'columnar']:
         msg = f'Invalid data format: `{output_data_format}`; output should be "json" or "columnar"' 
         raise ValueError( msg )
-    log.debug( '...and about to call create_foo.manager.manage_processing()' )
     manage_processing( output_data_format )  # Pass the parsed data to the create function
     return
--- code_to_package/search_collection_name/cli.py
+++ code_to_package/search_collection_name/cli.py
@@ -13,10 +13,12 @@ log = logging.getLogger( __name__ )
 
 def run( args: list ) -> None:
     log.debug( 'preparing arg-parser' )
-    parser = argparse.ArgumentParser(description="Update Foo Command")
-    parser.add_argument('--output', required=True, help='Data to update foo with')
+    parser = argparse.ArgumentParser( description='Parses search_collection_name arguments' )
+    parser.add_argument('--output', required=True, help='"json" or "columnar"')
     parsed_args = parser.parse_args(args)
     output_data_format: str = parsed_args.output
-
-    manage_processing( output_data_format )  # Pass the parsed data to the update function
+    if output_data_format not in ['json', 'columnar']:
+        msg = f'Invalid data format: `{output_data_format}`; output should be "json" or "columnar"' 
+        raise ValueError( msg )
+    manage_processing( output_data_format )  # Pass the parsed data to the create function
     return
