14472560447c975b7f430f92ae610dd001f772b6
Birkin James Diana <birkin.diana@gmail.com>
2024-09-10 23:21:19 -0400

    changes flag to --output

--- code_to_package/count_collections/cli.py
+++ code_to_package/count_collections/cli.py
@@ -13,13 +13,13 @@ log = logging.getLogger( __name__ )
 def run( args: list ) -> None:
     log.debug( 'in create_foo.cli.run(); preparing arg-parser' )
     parser = argparse.ArgumentParser( description="Create Foo Command" )
-    parser.add_argument('--data', required=True, help='"json" or "columnar"')
+    parser.add_argument('--output', required=True, help='"json" or "columnar"')
     parsed_args = parser.parse_args(args)
-    data_format: str = parsed_args.data
-    log.debug( f'parsed_args.data, ``{data_format}``' )
-    if data_format not in ['json', 'columnar']:
-        msg = f'Invalid data format: `{data_format}`; should be "json" or "columnar"' 
+    output_data_format: str = parsed_args.output
+    log.debug( f'parsed_args.data, ``{output_data_format}``' )
+    if output_data_format not in ['json', 'columnar']:
+        msg = f'Invalid data format: `{output_data_format}`; output should be "json" or "columnar"' 
         raise ValueError( msg )
     log.debug( '...and about to call create_foo.manager.manage_processing()' )
-    manage_processing( data_format )  # Pass the parsed data to the create function
+    manage_processing( output_data_format )  # Pass the parsed data to the create function
     return
