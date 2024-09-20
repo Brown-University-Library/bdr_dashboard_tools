d2d26b6400b09e1571457eaa6a6dccb6572f2e6d
Birkin James Diana <birkin.diana@gmail.com>
2024-09-19 22:09:13 -0400

    removes previous build artifacts.

--- build.sh
+++ build.sh
@@ -3,15 +3,22 @@
 set -e  # exits immediately on any error
 set -x  # print the command before executing it
 
-## check that the script is running from the correct directory
+## ensure script runs from the correct dir --------------------------
 DIR_NAME="bdr_dashboard_tools"
 if [ "$(basename "$PWD")" != "$DIR_NAME" ]; then
   echo "Error: Run this script from the '$DIR_NAME' directory."
   exit 1
 fi
 
-source ../env/bin/activate  # so we have access to shiv
+## remove pre-existing build stuff ----------------------------------
+rm -rf ./build
+rm -f ./bdr_dashboard_tools/bdr_dashboard_tools.egg-info
+rm -f ../bdr_dashboard_tools_binary
 
+## activate the venv for access to shiv -----------------------------
+source ../env/bin/activate  
+
+## build the binary -------------------------------------------------
 shiv --output-file ../bdr_dashboard_tools_binary --compressed --reproducible --console-script run_this  ./ --upgrade
 
 
